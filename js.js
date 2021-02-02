0d0f558d84714e19dd0446b4284289fdbde8aa30
PavPavv <pavleaks@gmail.com>
2021-02-02 12:34:53 +0400

    ADD: rest and spread, iterator, new file for short hooks

--- js.js
+++ js.js
@@ -552,7 +552,26 @@ function find() {
   console.log(book);
   console.log(book.item);
 }
-find();
+//find();
+
+// difference between ...rest and ... (spread operator)
+function restAndSpread() {
+  // ...rest
+  const showPerson = (name, surname, ...smthElse) => {
+    console.log("name: ", name);
+    console.log("surname: ", surname);
+    console.log("rest:", ...smthElse);
+  };
+  //showPerson("Jack", "Kerouac", "The Great", "author", "!");
+
+  // ...(spread)
+  let testNums = [1, 2, 3, 4, 5, -3];
+  const getMin = (arr) => {
+    return Math.min(...arr);
+  };
+  console.log(getMin(testNums));
+}
+//restAndSpread();
 
 ////////////////////////////////////////////////////////////////////////////////
 //  OBJECTS
@@ -634,9 +653,9 @@ function object() {
   // deep copy
   const deepCopy = JSON.parse(JSON.stringify(newObj2));
 
-  console.log("assign copyObj", copyObj);
-  console.log("lightCopy", lightCopy);
-  console.log("deepCopy", deepCopy);
+  // console.log("assign copyObj", copyObj);
+  // console.log("shallowCopy", shallowCopy);
+  // console.log("deepCopy", deepCopy);
 
   // JSON
   function json() {
@@ -694,151 +713,32 @@ function object() {
   //let newObj1 = Object.create({}); // create object with prototype
 
   //console.log(newObj1)
-}
-object();
 
-//  get Object Arrays
-function getObjectArrays() {
-  const myFavoriteAuthors = {
-    allAuthors: {
-      fiction: ["Leo Tolstoy", "Albert Camus", "Jack Kerouac"],
-      scienceFiction: ["Oldos Hucksley", "Isaak Asimov"],
-      fantasy: ["J.R.R. Tolkien"],
-    },
-  };
+  function interators() {
+    const range = {
+      band: 1,
+      established: 5,
+    };
 
-  function getObjArrs() {
-    let temp = [];
-    for (let key in myFavoriteAuthors["allAuthors"]) {
-      console.log(key); //  genres
-      console.log("------------------------------");
-      if (Array.isArray(myFavoriteAuthors["allAuthors"][key])) {
-        const array = myFavoriteAuthors["allAuthors"][key];
-
-        for (let value of array) {
-          temp.push(value);
-        }
-      }
-    }
-    //  authors
-    console.log(temp); // array
-    console.log(...temp); //  strings
-  }
-  getObjArrs();
-}
-//getObjectArrays();
-
-//  get Object arrays by internal method
-function getObjectArraysByInnerMethod() {
-  const myFavoriteAuthors = {
-    allAuthors: {
-      fiction: ["Leo Tolstoy", "Albert Camus", "Jack Kerouac"],
-      scienceFiction: ["Oldos Hucksley", "Isaak Asimov"],
-      fantasy: ["J.R.R. Tolkien"],
-    },
-
-    getAllAuthors() {
-      //  version 2
-      const test = Object.values(this.allAuthors).reduce(
-        (a, b) => a.concat(b),
-        []
-      );
-      return test;
-    },
-  };
-
-  console.log(myFavoriteAuthors.getAllAuthors());
-}
-//getObjectArraysByInnerMethod();
-
-//  Symbol.iterator will return an object called an iterator. This iterator will
-//  have a method called next which will return an object with keys value and done.
-function symbolIterator() {
-  const myFavoriteAuthors = {
-    allAuthors: {
-      fiction: ["Leo Tolstoy", "Albert Camus", "Jack Kerouac"],
-      scienceFiction: ["Oldos Hucksley", "Isaak Asimov"],
-      fantasy: ["J.R.R. Tolkien"],
-    },
-
-    // Old 'by hand' technique
-    // getAllAuthors() {
-    //   const authors = [];
-    //
-    //   for (const author of this.allAuthors.fiction) {
-    //     authors.push(author);
-    //   }
-    //
-    //   for (const author of this.allAuthors.scienceFiction) {
-    //     authors.push(author);
-    //   }
-    //
-    //   for (const author of this.allAuthors.fantasy) {
-    //     authors.push(author);
-    //   }
-    //
-    //   return authors;
-    // }
-
-    [Symbol.iterator]() {
-      //  Get all the authors in an array
-      const genresVals = Object.values(this.allAuthors);
-      //console.log(genresVals);
-
-      //  Store the current genre and author index
-      let currentAuthorIndex = 0;
-      let currentGenreIndex = 0;
-
+    range[Symbol.iterator] = function () {
       return {
-        // Implementation of next()
+        current: this.band,
+        last: this.established,
+
         next() {
-          //  authors according to current genre index
-          const authors = genresVals[currentGenreIndex];
-          //console.log('authors', authors);
-
-          // doNotHaveMoreAuthors is true when the authors array is exhausted.
-          // That is, all items are consumed.
-
-          const doNotHaveMoreAuthors = !(currentAuthorIndex < authors.length);
-          if (doNotHaveMoreAuthors) {
-            // When that happens, we move the genre index to the next genre
-            currentGenreIndex++;
-            // and reset the author index to 0 again to get new set of authors
-            currentAuthorIndex = 0;
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
           }
-          // if all genres are over, then we need tell the iterator that we
-          // can not give more values.
-
-          const doNotHaveMoreGenres = !(currentGenreIndex < genresVals.length);
-          if (doNotHaveMoreGenres) {
-            // Hence, we return done as true.
-            return {
-              value: undefined,
-              done: true,
-            };
-          }
-
-          // if everything is correct, return the author from the
-          // current genre and incerement the currentAuthorindex
-          // so next time, the next author can be returned.
-          return {
-            value: genresVals[currentGenreIndex][currentAuthorIndex++],
-            done: false,
-          };
         },
       };
-    },
-  };
+    };
 
-  for (const author of myFavoriteAuthors) {
-    //console.log(author);
+    for (let num of range) {
+      //console.log(num);
+    }
   }
-
-  console.log(...myFavoriteAuthors);
-
-  //  Not a solid technique of mine
-  // for (let item in myFavoriteAuthors) {
-  //   console.log(myFavoriteAuthors[item]);
-  // }
+  interators();
 }
-//symbolIterator();
+object();
