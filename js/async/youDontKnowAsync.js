624dade8d2b46956e681b15f821c361bf26a6e28
PavPavv <pavleaks@gmail.com>
2021-09-22 13:55:32 +0400

    добавил вопрос по SOLID, методам http, поправил предыдущие теоретические ответы по js

--- js/async/youDontKnowAsync.js
+++ js/async/youDontKnowAsync.js
@@ -32,44 +32,97 @@ function test() {
 
 //test();
 ////////////////////////////////////////////////////////////////////////////////
-jsTest();
-console.log("main log");
+function eventLoop() {
+  jsTest();
+  console.log("main log");
 
-function test() {
-  console.log("test");
-}
+  function test() {
+    console.log("test");
+  }
 
-const promise = new Promise((resolve, reject) => {
-  return resolve(console.log("resolve"));
-});
-promise
-  .then((res) => {
-    console.log("res");
-  })
-  .then((res) => {
-    setTimeout(() => {
-      console.log("1000");
-    }, 1000);
-  })
-  .then((res) => {
-    setTimeout(() => {
-      console.log("__");
-    });
-  })
-  .catch((err) => {
-    test();
-  })
-  .finally((res) => {
-    console.log("finally");
+  const promise = new Promise((resolve, reject) => {
+    return resolve(console.log("resolve"));
   });
 
-(function () {
-  console.log("super test");
-})();
+  const promise1 = new Promise((resolve, reject) => {
+    return resolve(console.log("kingslayer"));
+  });
 
-function jsTest() {
-  console.log("js");
+  const promise2 = new Promise((resolve, reject) => {
+    return resolve(promise1);
+  })
+    .then((res) => {
+      console.log("obey");
+    })
+    .finally((res) => {
+      console.log("finally0");
+    });
+
+  console.log("middle work");
+
+  promise
+    .then((res) => {
+      console.log("res");
+    })
+    .then((res) => {
+      setTimeout(() => {
+        console.log("1000");
+      }, 1000);
+    })
+    .then((res) => {
+      setTimeout(() => {
+        console.log("__");
+      });
+    })
+    .catch((err) => {
+      test();
+    })
+    .finally((res) => {
+      console.log("finally");
+    });
+
+  (function () {
+    console.log("super test");
+  })();
+
+  function jsTest() {
+    console.log("js");
+  }
 }
-
+//eventLoop();
 //  output:
 // js, main log, resolve, super test, res, finally, __, 1000
+
+let a1 = 1;
+let b1 = 2;
+
+[b1, a1] = [a1, b1];
+console.log(`a1: ${a1}`, `b1: ${b1}`);
+
+const person = {
+  greeting: function (name) {
+    return `Hi, I'm ${name}!`;
+  },
+};
+
+//console.log(person.greeting("Albert"));
+
+const albert = {};
+
+albert.__proto__ = person;
+console.log(albert);
+console.log(albert.greeting("Albert"));
+
+function trickySum(a) {
+  console.log(a);
+
+  return function (b) {
+    console.log(b + 1);
+
+    return function (c) {
+      console.log(c + c);
+    };
+  };
+}
+
+//console.log(trickySum(1)(2)(3));
